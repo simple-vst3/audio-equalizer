7b32ade1830f009a831f98dc5b099037bc5dc0fd
Drea Esposito <dreaesposito@hotmail.com>
2023-07-23 22:19:32 -0500

    Added grid lines

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -71,14 +71,20 @@ juce::Timer
     void timerCallback() override;
 
     void paint(juce::Graphics& g) override;
+    void resized() override;
 
 private:
     SimpleEQAudioProcessor& audioProcessor;
     juce::Atomic<bool> parametersChanged{ false };
 
     MonoChain monoChain;
-
+    
     void updateChain();
+
+    juce::Image background;
+
+    juce::Rectangle<int> getRenderArea();
+    juce::Rectangle<int> getAnalysisArea();
 };
 
 //==============================================================================
